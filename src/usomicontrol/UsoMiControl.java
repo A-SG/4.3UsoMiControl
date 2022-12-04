d567a47e0447c525e72d344abcf12db2377ba2bc
Alejandro Santos Gutiérrez <alejandrosantosgut@gmail.com>
2022-12-04 23:09:49 +0100

    Uso componente MiControl

---
+++ src/usomicontrol/UsoMiControl.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package usomicontrol;
+
+import controles.MiControl;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Alejandro Santos Gutiérrez
+ */
+public class UsoMiControl extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+        MiControl miControl = new MiControl();
+        miControl.setOnAction(new EventHandler<MouseEvent>() {
+            
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println("Has clickado el área roja");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(miControl);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Demo MiControl");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
